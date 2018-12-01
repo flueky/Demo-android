725d27ac4c6056fb5c683f94732a2d3242da175c
flueky <flueky@sina.com>
2018-12-01 14:00:24 +0800

    添加库工程和依赖配置

--- app/src/main/java/com/flueky/demo/MainActivity.java
+++ app/src/main/java/com/flueky/demo/MainActivity.java
@@ -3,10 +3,20 @@ package com.flueky.demo;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.facebook.common.util.Hex;
+
+import org.json.simple.JSONObject;
+
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Hex hex = new Hex();
+
+        JSONObject jsonObject = new JSONObject();
+
     }
 }
