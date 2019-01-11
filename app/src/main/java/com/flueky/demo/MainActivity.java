93a491e62283549903f31366cc9a02d401046689
flueky <flueky@sina.com>
2019-01-11 22:59:07 +0800

    删除无用依赖

--- app/src/main/java/com/flueky/demo/MainActivity.java
+++ app/src/main/java/com/flueky/demo/MainActivity.java
@@ -3,20 +3,11 @@ package com.flueky.demo;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.facebook.common.util.Hex;
-
-import org.json.simple.JSONObject;
-
-
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Hex hex = new Hex();
-
-        JSONObject jsonObject = new JSONObject();
 
     }
 }
