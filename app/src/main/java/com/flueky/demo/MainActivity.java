4ddd16a695973072c4ebc6342383fcee52ebd7fe
Flueky Zuo <flueky.zuo@zoom.us>
2021-10-31 18:45:36 +0800

    support androidx

--- app/src/main/java/com/flueky/demo/MainActivity.java
+++ app/src/main/java/com/flueky/demo/MainActivity.java
@@ -3,11 +3,13 @@ package com.flueky.demo;
 import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+
 import com.flueky.lib.Test;
 
 public class MainActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Test.test();
